c2d66863d31d19267cc30a92325b8f2fc6f89d94
Yuriy Sidorov <yuriy@digitaldali.ru>
2016-03-29 23:00:52 +0300

    Added mongo dump method

--- backuper.js
+++ backuper.js
@@ -320,6 +320,70 @@ SqlBackup.prototype.makeDump = function(callback) {
 	});
 };
 
+function MongoBackup(params) {
+	this.outputDir = params.outputDir;
+	this.dbs = params.dbs;
+	this.credentals = params.credentals;
+	this.keys = params.keys;
+
+	var curDate = new Date();
+
+	this.outputName = "mongodump_" + curDate.toISOString().replace(/T/, '_').replace(/\..+/, '').replace(/\:/g, '_');
+}
+
+MongoBackup.prototype.makeDump = function(callback) {
+	var self = this,
+		path = this.outputDir + '/' + this.outputName,
+		args = [];
+
+	if (this.credentals != null) {
+		args = ["-u", this.credentals.user, "-p" + this.credentals.passwd];
+	}
+
+	if (this.dbs !== "all") {
+		args.push("-dbs");
+		for (var i in this.dbs) {
+			args.push(this.dbs[i]);
+		}
+	}
+
+	for (var key in this.keys) {
+		args.push("--" + key, this.keys[key]);
+	}
+
+	args.push("-o", path);
+
+	console.log("Mongo args: " + JSON.stringify(args, null, 4));
+
+	var mongoDump = spawn("mongodump", args);
+
+	mongoDump.stdout.on('data', function(data) {
+		console.log("stdout: " + data);
+	});
+
+	mongoDump.stderr.on('data', function(data) {
+		console.log("stderr: " + data);
+	});
+
+	mongoDump.on('close', function(code) {
+		if (code === 0) {
+			makeArchive({
+				"inputDir" : '',
+				"inputFiles" : [self.outputDir + self.outputName],
+				"outputDir" : self.outputDir,
+				"outputFile" : "files_" + self.outputName
+			}, function(answ) {
+				deleteFolderRecursive(self.outputDir + self.outputName);
+
+				callback(answ);
+			});
+		}
+		else {
+			console.log("Smth went wrong at function 'makeDump'");
+		}
+	});
+};
+
 function Backuper(configs) {
 	this.dirs = [];
 	for (var numDir in configs.dirs) {
@@ -331,6 +395,8 @@ function Backuper(configs) {
 		"user" : configs.sql.credentals.user,
 		"passwd" : configs.sql.credentals.passwd
 	};
+
+	this.mongoInfo = configs.mongo;
 
 	this.outputDir = configs.outputDir;
 
@@ -346,9 +412,12 @@ function Backuper(configs) {
 	fs.mkdirSync(this.outputDir + '/' + this.tmpDir);
 }
 
-Backuper.prototype.sqlBackups = function(callback) {
+Backuper.prototype.dbBackups = function(type, callback) {
 	var self = this,
-		sqlInst = new SqlBackup({
+		dbInst = null;
+
+	if (type === "sql") {
+		dbInst = new SqlBackup({
 			"outputDir" : this.outputDir + this.tmpDir + '/',
 			"dbs" : this.mySqlInfo.dbs,
 			"credentals" : {
@@ -356,8 +425,18 @@ Backuper.prototype.sqlBackups = function(callback) {
 				"passwd" : this.mySqlInfo.passwd
 			}
 		});
+	}
+	else if (type === "mongo") {
+		dbInst = new MongoBackup({
+			"outputDir" : this.outputDir + this.tmpDir + '/',
+			"dbs" : this.mongoInfo.dbs,
+			"credentals" : this.mongoInfo.credentals,
+			"keys" : this.mongoInfo.keys
+		});
+	}
 
-	sqlInst.makeDump(function(resp) {
+
+	dbInst && dbInst.makeDump(function(resp) {
 		if (resp.res === "ok") {
 			fs.rename(self.outputDir + '/' + self.tmpDir + '/' +resp.outputArchive, self.outputDir + '/' + resp.outputArchive, function(err) {
 				if (err) {
@@ -477,7 +556,7 @@ Backuper.prototype.remoteBackup = function() {
 }
 
 Backuper.prototype.startBackups = function(callback) {
-	var numBackups = 2,
+	var numBackups = 3,
 		self = this;
 
 	var finalFunction = function(resp) {
@@ -497,7 +576,11 @@ Backuper.prototype.startBackups = function(callback) {
 		finalFunction(resp);
 	});
 
-	this.sqlBackups(function(resp) {
+	this.dbBackups("sql", function(resp) {
+		finalFunction(resp);
+	});
+
+	this.dbBackups("mongo", function(resp) {
 		finalFunction(resp);
 	});
 };
